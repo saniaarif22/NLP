9d5db4c2f2519beef891d60264a8d9df953fbd7c
Sania Arif <sania.arif@columbia.edu>
2016-04-30 01:32:29 -0400

    added clock to measure time

--- Machine Translation/main.py
+++ Machine Translation/main.py
@@ -2,14 +2,14 @@ from nltk.corpus import comtrans
 import A
 import B
 import EC
-
+import time
 
 if __name__ == '__main__':
-
+    time.clock()
     aligned_sents = comtrans.aligned_sents()[:350]
     A.main(aligned_sents)
-
+    print "Part A time: " + str(time.clock()) + ' sec'
     #A.testmain(aligned_sents)
     B.main(aligned_sents)
-
+    print "Part B time: " + str(time.clock()) + ' sec'
     EC.main(aligned_sents)
